18083db8d3c7e531ca38857f26b8a6f2b74b4009
rlagnl <250989770@qq.com>
2020-07-06 21:08:47 +0800

    Squashed 'sub/libs/' changes from 68b568c..c41c8d5
    c41c8d5 build(webpack build): delete of test code
    7585ffe build(global): init project
    
    git-subtree-dir: sub/libs
    git-subtree-split: c41c8d5f4b9ada0fb572be0a4bab8866e51f30dd

---
+++ config/test.ts
@@ -0,0 +1,17 @@
+import './_window'
+import { object2string } from './utils'
+
+const handler = {
+	get: function (obj: any, prop: any) {
+		console.log('get', obj, prop, _window)
+		if (prop === 'innerWidth') {
+			return 100
+		}
+		return _window[prop]
+	}
+}
+
+const object = 'new Proxy({}, ' + object2string(handler) + ')'
+console.log('=== handler ===', object)
+
+export default object
